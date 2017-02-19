bdea63d30f6b86a6bc9c20878ebea0ab01ff5743
ohamax <junji.ohama@toolbit.org>
2017-02-19 21:23:13 +0900

    Add README.md and static link option in extconf.rb

---
+++ bindings/ruby/README.md
@@ -0,0 +1,34 @@
+Ruby bindings
+====
+
+## Descripton
+
+Build library of Toolbit Interface for Ruby.
+
+
+## Require
+
+ruby
+  32bit version is recommended
+
+swig
+  http://www.swig.org/
+
+
+## Build
+
+cd swig
+./build.sh
+tbi.so will be created and copied to ../samples folder
+
+
+## Test
+
+cd ../samples
+ruby chopper_test.rb
+ruby luke_test.rb
+
+
+## Reference
+
+
--- bindings/ruby/swig/extconf.rb
+++ bindings/ruby/swig/extconf.rb
@@ -1,8 +1,11 @@
 require 'mkmf'
 
 if RUBY_PLATFORM.downcase =~ /mswin(?!ce)|mingw|cygwin|bccwin/
-  puts "Windows"
-  $libs += " -lstdc++ -lsetupapi"
+  puts "Windows"#
+  $libs += "-lsetupapi"
+# The following options might be useful to distribute it for non MinGW users
+# $libs += "-lsetupapi -static-libstdc++ -static-libgcc"
+
 else
   puts "Linux"
   $libs += " -lstdc++ -lusb-1.0 -lpthread"
