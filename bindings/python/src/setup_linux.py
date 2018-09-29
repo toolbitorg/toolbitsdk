f40915d16543afeb202a103aaaa1829cfcf4d927
ohamax <junji.ohama@toolbit.org>
2018-09-29 04:38:33 -0700

    Update python bindings for linux to follow changes of src files

--- bindings/python/src/setup_linux.py
+++ bindings/python/src/setup_linux.py
@@ -1,9 +1,10 @@
 from distutils.core import setup, Extension
 
 tbi_module = Extension('_tbi',
-                    sources = ['tbi_wrap.cxx', 'tbi_core.cpp', 'tbi_device.cpp', 'tbi_service.cpp', 'attribute.cpp', 'tbit.cpp', 'hid.c', 'basicdemo.cpp', 'chopper.cpp', 'luke.cpp', 'tbi_device_manager.cpp', 'adc.cpp', 'gpio.cpp', 'i2c.cpp'],
+                    sources = ['tbi_wrap.cxx', 'tbi_core.cpp', 'tbi_device.cpp', 'tbi_device_manager.cpp', 'tbi_service.cpp', 'attribute.cpp', 'tbit.cpp', 'hid.c', 'basicdemo.cpp', 'chopper.cpp', 'luke.cpp', 'adc.cpp', 'adc_hw.cpp', 'gpio_hw.cpp', 'i2c_hw.cpp', 'pin.cpp'],
                     include_dirs=['/usr/include/libusb-1.0'],
                     libraries=['usb-1.0', 'udev'],
+                    extra_compile_args=["-std=gnu++11"],
                 )
 
 setup (name = 'tbi',
