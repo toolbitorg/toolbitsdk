f398c670f35d7f01516ba89e78a9bd1503f69913
ohamax <junji.ohama@toolbit.org>
2018-10-04 04:33:59 -0700

    Modify ruby bindings for linux

--- bindings/ruby/extconf.rb
+++ bindings/ruby/extconf.rb
@@ -8,7 +8,9 @@ if RUBY_PLATFORM.downcase =~ /mswin(?!ce)|mingw|cygwin|bccwin/
 
 else
   puts "Linux"
-  $libs += " -lstdc++ -lusb-1.0 -lpthread"
+  $CXXFLAGS = " -g -std=c++11 "
+  $libs += " -lusb-1.0 -ludev"
+  #  $libs += " -lstdc++ -lusb-1.0 -ludev"
 end
 
 create_makefile("tbi")
