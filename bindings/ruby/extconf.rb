750f0efa7d9ffc10adf475f3eae41d27ebee1caf
Ohamax <junji.ohama@toolbit.org>
2018-11-05 22:44:00 +0900

    Add all bindings for macos

--- bindings/ruby/extconf.rb
+++ bindings/ruby/extconf.rb
@@ -1,15 +1,23 @@
 require 'mkmf'
 
 if RUBY_PLATFORM.downcase =~ /mswin(?!ce)|mingw|cygwin|bccwin/
-  puts "Windows"#
+  puts "Windows"
 #  $libs += "-lsetupapi"
 # The following options might be useful to distribute it for non MinGW users
   $libs += "-lsetupapi -static-libstdc++ -static-libgcc"
 
-else
+elsif RUBY_PLATFORM.downcase =~ /linux/
   puts "Linux"
   $CXXFLAGS = " -g -MMD -MP  -std=c++11 "
   $libs += " -lstdc++ -lusb-1.0 -ludev"
+
+elsif RUBY_PLATFORM.downcase =~ /darwin/
+  puts "macOS"
+  $CXXFLAGS = " -g -MMD -MP  -std=c++11 "
+  $libs += " -lstdc++ -framework IOKit -framework Carbon"
+
+else
+  puts "We're running under an unknown operating system."
 end
 
 create_makefile("tbi")
