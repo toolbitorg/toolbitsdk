bdecd5b203dfb20805805b264efde374fbbd85da
ohamax <junji.ohama@toolbit.org>
2019-06-16 14:44:56 +0900

    Revert "Support event handler for event packet"
    This reverts commit 786a91da8d5be3babd2016155276cb683bab8067.

--- src/tbit.cpp
+++ src/tbit.cpp
@@ -1,18 +1,6 @@
 #include "tbi_device_manager.h"
 #include "tbit.h"
 
-#include <iostream>
-
-using namespace std;
-
-void eventHandler(tbiPacket pckt)
-{
-	cout << "Get event notify packet: ";
-
-	for (int i = 0; i < (pckt.dat[0] & 0x3F); i++)
-		cout << unsigned(pckt.dat[i]);
-	cout << endl;
-}
 
 Tbit::Tbit() :
 	gpiohw(mTbiService, ATT_GPIO0_BASE),
@@ -21,10 +9,7 @@ Tbit::Tbit() :
 {
 	TbiDeviceManager devm;
 
-//	if (open(devm.getPath("Tbit"))) {
-	if (open(devm.getPath("CHOPPER HUB"))) {
-
-		this->mTbiService->bindEventHandler(eventHandler);
+	if (open(devm.getPath("Tbit"))) {
 
 	}
 
