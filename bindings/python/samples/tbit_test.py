a0179d52c367428ebc86a4ff766b603dfd77d537
ohamax <junji.ohama@toolbit.org>
2020-01-06 14:30:04 +0900

    Update python bindings and samples

--- bindings/python/samples/tbit_test.py
+++ bindings/python/samples/tbit_test.py
@@ -2,8 +2,9 @@ import tbi
 import time
 
 tbit = tbi.Tbit()
+tbit.open()
 
-print tbit.gpiohw.read()
+print(tbit.gpiohw.read())
 tbit.gpiohw.digitalWrite(15, True)
 time.sleep(0.5)
 tbit.gpiohw.digitalWrite(15, False)
@@ -16,12 +17,10 @@ adcpin = tbi.Adc(tbit, 4)
 time.sleep(0.5)
 led.on()
 outpin.on()
-print inpin.value()
-print adcpin.value()
+print(inpin.value())
+print(adcpin.value())
 
 time.sleep(0.5)
 led.off()
 outpin.off()
-print inpin.value()
-print adcpin.value()
-
+print(adcpin.value())
