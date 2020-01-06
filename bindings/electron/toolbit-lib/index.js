2f2072e061abd14f620f4812e41400eed906c9c4
ohamax <junji.ohama@toolbit.org>
2020-01-06 23:18:21 +0900

    Fix a typo of toolbit-lib/index.js

--- bindings/electron/toolbit-lib/index.js
+++ bindings/electron/toolbit-lib/index.js
@@ -1,5 +1,5 @@
 if(process.platform==='linux') {
-  module.exports = require('./lib/tbi-lunux.node');
+  module.exports = require('./lib/tbi-linux.node');
 } else if (process.platform==='darwin') {
   module.exports = require('./lib/tbi-macos.node');
 } else if (process.platform==='win32') {
