1858239d633b66a739138e044fb8f4549233230d
ohamax <toolbitorg@gmail.com>
2024-02-28 18:41:11 +0900

    Modify makefiles to migrate to libusb/hidapi

--- bindings/python/toolbit-lib/build.bat
+++ bindings/python/toolbit-lib/build.bat
@@ -6,9 +6,9 @@ copy ..\..\..\src\*.cpp toolbit
 mkdir toolbit\macos
 mkdir toolbit\linux
 mkdir toolbit\mswin
-copy ..\..\..\src\HIDAPI\linux\hid.c toolbit\linux
-copy ..\..\..\src\HIDAPI\mac\hid.c toolbit\macos
-copy ..\..\..\src\HIDAPI\windows\hid.c toolbit\mswin
+copy ..\..\..\src\hidapi\linux\hid.c toolbit\linux
+copy ..\..\..\src\hidapi\mac\hid.c toolbit\macos
+copy ..\..\..\src\hidapi\windows\hid.c toolbit\mswin
 copy ..\..\toolbit.i toolbit
 cd toolbit; swig -c++ -python toolbit.i
 move toolbit\toolbit.py toolbit\__init__.py
