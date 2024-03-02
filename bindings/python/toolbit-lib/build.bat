302ee2a1dbcfab7ccba8aa6b677ef631e2ebb3b5
ohamax <toolbitorg@gmail.com>
2024-03-02 12:36:17 +0900

    Modify build.bat for python bindings to migrate to libusb/hidapi

--- bindings/python/toolbit-lib/build.bat
+++ bindings/python/toolbit-lib/build.bat
@@ -9,7 +9,17 @@ mkdir toolbit\mswin
 copy ..\..\..\src\hidapi\linux\hid.c toolbit\linux
 copy ..\..\..\src\hidapi\mac\hid.c toolbit\macos
 copy ..\..\..\src\hidapi\windows\hid.c toolbit\mswin
+copy ..\..\..\src\hidapi\windows\hid.c toolbit\mswin
+copy ..\..\..\src\hidapi\windows\hidapi_winapi.h toolbit\mswin
+copy ..\..\..\src\hidapi\windows\hidapi_cfgmgr32.h toolbit\mswin
+copy ..\..\..\src\hidapi\windows\hidapi_hidclass.h toolbit\mswin
+copy ..\..\..\src\hidapi\windows\hidapi_hidsdi.h toolbit\mswin
+copy ..\..\..\src\hidapi\windows\hidapi_hidpi.h toolbit\mswin
+copy ..\..\..\src\hidapi\windows\hidapi_descriptor_reconstruct.c toolbit\mswin
+copy ..\..\..\src\hidapi\windows\hidapi_descriptor_reconstruct.h toolbit\mswin
 copy ..\..\toolbit.i toolbit
-cd toolbit; swig -c++ -python toolbit.i
-move toolbit\toolbit.py toolbit\__init__.py
+cd toolbit
+swig -c++ -python toolbit.i
+move toolbit.py __init__.py
+cd ..
 python setup.py build_ext --inplace
